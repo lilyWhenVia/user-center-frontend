7d5c731c0e5853c2bc96a03da5704e0f0b3675cd
lily via <lily228250998@gmail.com>
2023-10-10 11:53:21 +0800

    first init

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const SYSTEM_LOGO = "https://github.com/lilyWhenVia/learngit/blob/master/images/c4d0672515a5b423d59d0f6724ed9d9b.png?raw=true";
+
+/**
+ * 知识星球文档地址
+ */
+export const PLANET_LINK = "https://docs.qq.com/doc/DUG93dVNHbVZjZXpo";
+export const SUCCESS_CODE = "2000";
+export const SYSTEM_ERR_CODE = "5000";
+export const UN_AUTH_CODE = "4010";
+export const UN_LOGIN_CODE = "4011";
+export const ACCOUNT_NOT_FOUND_CODE = "3000";
+export const WRONG_PARAM_ERR_CODE = "3";
+export const USER_LONGING_STATE = "user";
